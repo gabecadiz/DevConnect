e363b031027f9f048077ae12fd636b429fd83911
Computer <gabecadiz@hotmail.com>
2019-07-28 12:39:30 -0400

    created dashboard component, protected route higher order component, loading spinner component, created profile reducer, actions and types. added profile logic to dashboard to display certain information

---
+++ client/src/components/layouts/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <i className='fa fa-spinner fa-spin fa-4x' />
+    </div>
+  );
+};
+
+export default Spinner;
